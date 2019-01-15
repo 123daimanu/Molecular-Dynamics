54fe864a5e01f9e5fab84b7ccd60670e449fb116
123manudai <excuzme.hemanta@gmail.com>
2019-01-15 15:10:10 -0500

    minor changes made :: the program runs correctly

--- HardSphere/run/md.cpp
+++ HardSphere/run/md.cpp
@@ -2,20 +2,22 @@
 
 
 #include "../sim/initialize.hpp"
-
+#include "../sim/atom.hpp"
+#include "../maths/Vector3.hpp"
 using namespace std;
 
 
 int main()
 {
-  int number_of_atoms=10;
+  int number_of_atoms=100;
   initialize system(number_of_atoms);
-  system.setnumber(number_of_atoms);
   system.Initial();
-  cout<<"address of system "<<(system.systemAtoms)<<endl;
+    cout<<"address of system "<<(system.systemAtoms)<<endl;
 
-  /*for(int i=1;i<=number_of_atoms;i++)
+  for(int i=1;i<=number_of_atoms;i++)
   {
-    cout<<"Position of "<< i <<"atom : " << (system.systemAtoms[i]).getPosition().x<< endl;
-  }*/
+    cout<<"Position of "<< i <<"atom : " << (system.systemAtoms[i]).getVelocity().x<< endl;
+  }
+
+
 }
