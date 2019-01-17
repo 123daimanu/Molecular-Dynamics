9af57b3be1cfd2f0f0d2d11974aa5112f202d046
123manudai <excuzme.hemanta@gmail.com>
2019-01-17 17:05:06 -0500

    CMakeList.txt changed

--- HardSphere/src/sim/simulation.cpp
+++ HardSphere/src/sim/simulation.cpp
@@ -3,6 +3,7 @@
 
 #include "simulation.hpp"
 #include "initialize.hpp"
+#include "atom.hpp"
 #include "../maths/Vector3.hpp"
 
 bool simulation::collisionFlag(atom a, atom b)
@@ -10,21 +11,21 @@ bool simulation::collisionFlag(atom a, atom b)
   Vector3 v12 = V12(a.getVelocity(),b.getVelocity());
   Vector3 r12 = V12(a.getPosition(),b.getPosition());
   float dotprod = dot(r12,v12);
-  if dotprod<0
+  if (dotprod<0)
   {
-    float disc = dotprod*dotprod - v12.square()*(r12.squre()-sigma*sigma)
-    if disc >=0 return True;
+    float disc = dotprod*dotprod - v12.square()*(r12.square()-sigma*sigma)
+    if (disc >=0) return True;
   }
-  return False;
+  return false;
 }
 float simulation::collisionTime(atom a, atom b)
 {
   Vector3 v12 = V12(a.getVelocity(),b.getVelocity());
   Vector3 r12 = V12(a.getPosition(),b.getPosition());
   float dotprod = dot(r12,v12);
-  float disc = dotprod*dotprod - v12.square()*(r12.square()-sigma*sigma)
+  float disc = dotprod*dotprod - v12.square()*(r12.square()-sigma*sigma);
   float deltaT;
-  deltaT = (-dotprod - sqrt(disc))/(v12.square())
+  deltaT = (-dotprod - sqrt(disc))/(v12.square());
   return simTime+deltaT;
 
 
@@ -32,19 +33,19 @@ float simulation::collisionTime(atom a, atom b)
 }
 void velocityUpdate(atom *a, atom *b)
 {
-  Vector3 v12 = V12(*a.getVelocity(),*b.getVelocity());
-  Vector3 r12 = V12(*a.getPosition(),*b.getPosition());
+  Vector3 v12 = V12(a->getVelocity(),b->getVelocity());
+  Vector3 r12 = V12(a->getPosition(),b->getPosition());
   float dotprod = dot(r12,v12);
   float r12Squ =  r12.square();
   Vector3 deltaV = r12*(dotprod/r12Squ);
-  *a.setVelocity(*a.getVelocity()-deltaV);
-  *b.setVelocity(*b.getVelocity()+deltaV);
+  a->setVelocity(a->getVelocity()-deltaV);
+  b->setVelocity(b->getVelocity()+deltaV);
 
 }
 void positionUpdate(atom *a)
 {
-  Vector3 posNew = *a.getPosition()+(*a).getVelocity()*(simTime-(*a).getTinit());
-  *a.setPosition(posNew);
+  Vector3 posNew = a->getPosition()+a->getVelocity()* ( simTime - a->getTinit());
+  a->setPosition(posNew);
 
 }
 void collisionTimeUpdate();
